5037365539003f5cdb6782c092615e162a80436e
tiuphun <phuong.nt210692@sis.hust.edu.vn>
2024-10-20 16:58:20 +0700

    hw3 done

--- homework3/exercise1/client.c
+++ homework3/exercise1/client.c
@@ -24,4 +24,51 @@ int main(int argc, char *argv[])
 
     // Create UDP socket
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0)
+    if (sockfd < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Define server address
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    // Convert addr -> binary
+    if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0) {
+        perror("Invalid IP address");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Send & receive messages
+    while (1) {
+        printf("Enter a string: ");
+        fgets(input, sizeof(input), stdin);
+        input[strlen(input)-1] = '\0'; // remove newline char
+        if (strlen(input) == 0 || strcmp(input, "***") == 0) {
+            break;
+        }
+        sendto(sockfd, input, strlen(input), 0, (const struct sockaddr *)&server_addr, sizeof(server_addr));
+
+        int n = recvfrom(sockfd, response, MAXBUFLEN, 0, NULL, NULL);
+        response[n] = '\0';  // Null-terminate the received string
+
+        // Check if it's an error message
+        if (strncmp(response, "Error", 5) == 0) {
+            // If error, print it and skip receiving the second response
+            printf("%s\n", response);
+            continue;  // Go back to get a new input
+        }
+
+        // If it's not an error, print the alphabetic part
+        printf("%s\n", response);
+
+        n = recvfrom(sockfd, response, MAXBUFLEN, 0, NULL, NULL);
+        response[n] = '\0';  // Null-terminate the received string
+        printf("%s\n", response);
+        
+    }
+
+    close(sockfd);
+    return 0;
+}
